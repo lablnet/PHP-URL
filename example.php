4c1dd8ff4def34fe1a9e53ad7f70a27bd671eb25
malikumer <lablnet01@gmail.com>
2018-04-15 18:39:26 +0500

    fix form issue

--- example.php
+++ example.php
@@ -4,63 +4,150 @@
 	
 	require_once( 'classes/Url.php' );
 
-	$test = "how are you friends https://github.com/";
+	if(isset($_POST['submit'])){
 
-	$getSite = new URL;
+		$test = $_POST['url'];
+
+		$getSite = new URL;
 
 
-	//for performance improvement
-	$getSite->SetConnTime(8);
+		//for performance improvement
+		$getSite->SetConnTime(8);
 
-	$filter_url = $getSite->FilterUrl($test);
+		$filter_url = $getSite->FilterUrl($test);
 
-	$getSite->SetUrl( $filter_url[0][0] );
+		$getSite->SetUrl( $filter_url[0][0] );
 
-	$data = $getSite->GetData();
+		$data = $getSite->GetData();
 
-	//getting all data into array
+		//getting all data into array
 
-	//print_r($data);
+		//print_r($data);
 
+		
+
+		echo $data['title'];
+
+
+		echo "<br>";
+
+		echo $data['url'];
+
+		echo "<br>";
+
+		echo $data['slug'];
+
+		echo "<br>";
+
+		//keywords
+
+		echo $data['keywords'];
+
+		echo "<br>";
+
+		//description
+
+		//echo $data['description'];
+		print_r($data['description']);
+		echo "<br>";
+
+		//echo $data['screenshot'];
+
+		//printing the screenshort
+		echo '<img src="data:image/png;base64,'. $data['screenshot'].'" alt="Red dot" />';
+
+		echo "<br>";
+
+		//image if you want print all images using foreach loop
+
+		if(isset($data['images'][0][1])){
+
+			echo $data['images'][0][1].'/>';
+
+		}
 	
-
-	echo $data['title'];
-
-
-	echo "<br>";
-
-	echo $data['url'];
-
-	echo "<br>";
-
-	echo $data['slug'];
-
-	echo "<br>";
-
-	//keywords
-
-	echo $data['keywords'];
-
-	echo "<br>";
-
-	//description
-
-	//echo $data['description'];
-	print_r($data['description']);
-	echo "<br>";
-
-	//echo $data['screenshot'];
-
-	//printing the screenshort
-	echo '<img src="data:image/png;base64,'. $data['screenshot'].'" alt="Red dot" />';
-
-	echo "<br>";
-
-	//image if you want print all images using foreach loop
-
-	if(isset($data['images'][0][1])){
-
-		echo $data['images'][0][1].'/>';
-
 	}
+
+?>
+
+<form action='' method='post'>
+	<input type="text" name="url">
+	<br/>
+	<input type="submit" name="submit">
+</form><?php
+
+	//call the class using require_once //better for error checking
 	
+	require_once( 'classes/Url.php' );
+
+	if(isset($_POST['submit'])){
+
+		$test = $_POST['url'];
+
+		$getSite = new URL;
+
+
+		//for performance improvement
+		$getSite->SetConnTime(8);
+
+		$filter_url = $getSite->FilterUrl($test);
+
+		$getSite->SetUrl( $filter_url[0][0] );
+
+		$data = $getSite->GetData();
+
+		//getting all data into array
+
+		//print_r($data);
+
+		
+
+		echo $data['title'];
+
+
+		echo "<br>";
+
+		echo $data['url'];
+
+		echo "<br>";
+
+		echo $data['slug'];
+
+		echo "<br>";
+
+		//keywords
+
+		echo $data['keywords'];
+
+		echo "<br>";
+
+		//description
+
+		//echo $data['description'];
+		print_r($data['description']);
+		echo "<br>";
+
+		//echo $data['screenshot'];
+
+		//printing the screenshort
+		echo '<img src="data:image/png;base64,'. $data['screenshot'].'" alt="Red dot" />';
+
+		echo "<br>";
+
+		//image if you want print all images using foreach loop
+
+		if(isset($data['images'][0][1])){
+
+			echo $data['images'][0][1].'/>';
+
+		}
+	
+	}
+
+?>
+
+<form action='' method='post'>
+	<input type="text" name="url">
+	<br/>
+	<input type="submit" name="submit">
+</form>
